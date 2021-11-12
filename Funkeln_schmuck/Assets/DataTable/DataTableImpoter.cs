a6d4ec6fe6a0493068e857dba374c5b8816a61f8
cfe059 <20j70009oy@edu.tca.ac.jp>
2021-11-12 16:58:19 +0900

    ReMain
    ver1.15 + git.ignore

---
+++ Funkeln_schmuck/Assets/DataTable/DataTableImpoter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class CharacterTable
+{
+    public int ID;
+    public string CharacterName;
+    public float HP;
+    public float Attack;
+    public float Defence;
+    public float Speed;
+    public float AttackSpeed;
+    public float AttackRange;
+    public float skillGage;
+    public float GageHeal;
+    public float Rare;
+    public float Level;
+    public float Escape;
+    public float CriticalPower;
+    public float CriticalPersent;
+
+}
---
+++ Funkeln_schmuck/Assets/Script/TextBoxSwitch.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject TextBox1;
+    [SerializeField] GameObject TextBox2;
+    [SerializeField] GameObject TextBox3;
+    [SerializeField] GameObject TextBox4;
+    [SerializeField] int min;
+    [SerializeField] int max;
+    int value = 0;
+    int ran = 0;
+    // Start is called before the first frame update
+    public void OnTapTextSwitch()
+    {
+        value = ran;
+
+        while(value == ran)
+        {
+            ran = Random.Range(min, max);
+        }
+        switch (ran)
+        {
+            case 1:
+                TextBox1.SetActive(true);
+                TextBox2.SetActive(false);
+                TextBox3.SetActive(false);
+                TextBox4.SetActive(false);
+                Debug.Log("Text1");
+                break;
+            case 2:
+                TextBox1.SetActive(false);
+                TextBox2.SetActive(true);
+                TextBox3.SetActive(false);
+                TextBox4.SetActive(false);
+                Debug.Log("Text2");
+                break;
+            case 3:
+                TextBox1.SetActive(false);
+                TextBox2.SetActive(false);
+                TextBox3.SetActive(true);
+                TextBox4.SetActive(false);
+                Debug.Log("Text3");
+                break;
+            case 4:
+                TextBox1.SetActive(false);
+                TextBox2.SetActive(false);
+                TextBox3.SetActive(false);
+                TextBox4.SetActive(true);
+                Debug.Log("Text4");
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+
