9d5bcb7308481e7308d2eedf2bc7f92326418128
KDavidx <77654793+KDavidx@users.noreply.github.com>
2021-11-13 19:03:42 +0900

    debug FPS

---
+++ Assets/TestChar/Debug_fps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_fps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // public Text fpsText;
+    public float deltaTime;
+ 
+    void Update () {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        //fpsText.text = Mathf.Ceil (fps).ToString ();
+        Debug.Log(Mathf.Ceil(fps).ToString());
+    }
+}
