5299b01c8ee63cfeac7a64070dccbc0a0b52e304
cfe059 <45842434+cfe059@users.noreply.github.com>
2021-11-24 09:30:25 +0900

    DataTable整理

---
+++ Assets/DataTable/ImpoerScript/DataTableImpoter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class CharacterTable
+{
+    public int ID;
+    public string CharacterName;
+    public float HP;
+    public float Attack;
+    public float Defence;
+    public float Speed;
+    public float AttackSpeed;
+    public float AttackRange;
+    public float skillGauge;
+    public float GaugeHealing;
+    public float Rare;
+    public float Level;
+    public float Escape;
+    public float CriticalDMG;
+    public float CriticalHit;
+}
+
+// public class StageTable
+// {
+//     public int MapID;
+//     public string MapName;
+//     public int StageCount;
+//     public int EnrmyPlace;
+//     public bool BossFlag;
+//     public int EnemyNum1;
+//     public int EnemyNum2;
+//     public int EnemyNum3;
+//     public int EnemyNum4;
+//     public int EnemyNum5;
+// }
+//
+// public class Dialogues
+// {
+//     public int DialogueID;
+//     public string DialogueName;
+//     public string Dialogue;
+//     public int CharacterID1;
+//     public int CharacterID2;
+//     public int CharacterID3;
+//     public int Continue;
+//     public int SkillID;
+//     public int RouletteID;
+// }
