32a0bad0fec4993efbb221bb668e32ffcf331604
cfe059 <cfe059@yahoo.co.jp>
2022-01-10 17:29:51 +0900

    修正

--- Assets/Script/Party/Party_Data.cs
+++ Assets/Script/Party/Party_Data.cs
@@ -37,8 +37,6 @@ public class Party_Data : MonoBehaviour
                     if (GManager.instance.character_pos[i] == data.Character[j].ID)
                     {
 
-                        _Party_Pos_Frame[i].sprite = Resources.Load<Sprite>("Party_UI/EmptyFrame");
-
                         switch (data.Character[j].Rare)
                         {
                             case 1:
@@ -54,6 +52,7 @@ public class Party_Data : MonoBehaviour
                                 _Party_Pos_backGround[i].sprite = Resources.Load<Sprite>("Party_UI/Rainbow");
                                 break;
                             default:
+                                _Party_Pos_Frame[i].sprite = Resources.Load<Sprite>("Party_UI/EmptyFrame");
                                 break;
                         }
                     }
@@ -104,6 +103,7 @@ public class Party_Data : MonoBehaviour
                     _Party_Pos_backGround[_TryChange].sprite = Resources.Load<Sprite>("Party_UI/Rainbow");
                     break;
                 default:
+                    _Party_Pos_Frame[i].sprite = Resources.Load<Sprite>("Party_UI/EmptyFrame");
                     break;
             }   
         }
