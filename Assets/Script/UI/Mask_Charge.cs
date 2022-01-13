244c6a31c9682e3d0f6e07955ccd76e2d1393983
cfe059 <cfe059@yahoo.co.jp>
2022-01-14 02:39:59 +0900

    UISlide

---
+++ Assets/Script/UI/Mask_Charge.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mask_Charge : MonoBehaviour
+{
+    [SerializeField] private GameObject Black;
+    [SerializeField] private GameObject Gage;
+
+    [SerializeField] private GameObject Manager;
+
+    private float GageMax;
+    private float GageNow;
+
+    private void Update()
+    {
+        
+    }
+}
