bb583272f396bf9c4cc59a2bf423eea8069e0bdb
KDavidx <77654793+KDavidx@users.noreply.github.com>
2022-01-14 18:58:45 +0900

    cutin

--- Assets/Script/Roulette/CutIn_Manager.cs
+++ Assets/Script/Roulette/CutIn_Manager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,26 +13,72 @@ public class CutIn_Manager : MonoBehaviour
 
     [SerializeField] private Roulette _roulette;
 
-    private List<Vector3> pos;
+    [SerializeField] private List<Vector3> pos;
+    private bool[] D_Slider = {false, false };
+    private bool[] D_Slider_Done = {false, false };
+    private void Update()
+    {
+        for (int i = 0; i < D_Slider.Length; i++)
+        {
+            if (D_Slider[i])
+            {
+                _Slider(i+1);
+                if (i == 1)
+                {
+                    Disable_Img(0);
+                }
+            }
+    
+            
+        }
 
+        for (int i = 0; i < D_Slider.Length; i++)
+        {
+            if (D_Slider_Done[i])
+            {
+                StartCoroutine(CutIn_Disp(i+1,1));
+                
+            }
+        }
+
+        if (D_Slider_Done[1])
+        {
+            D_Slider_Done[0] = false;
+            D_Slider_Done[1] = false;     
+            D_Slider[0] = false;
+            D_Slider[1] = false;
+            
+            StartCoroutine(CutIn_Fin(2));
+        }
+    }
+
+    void Disable_Img(int i)
+    {
+        Frame[i].enabled = false;
+        Character[i].enabled = false;
+        Dialogue[i].enabled = false;
+    }
     public void Dialogue_CutIn()
     {
         CutIn_Init();
-
-        int y = 0;
-        for (int i = 0; i < Frame.Count; i++)
-        {
-            StartCoroutine(CutIn_Disp(i,y));
-            StartCoroutine(CutIn_Slide(i,y));
-            y += 2;
-        }
-
-        StartCoroutine(CutIn_Fin(3));
+       
+        // int y = 0;
+        // for (int i = 0; i < Frame.Count; i++)
+        // {
+        //     StartCoroutine(CutIn_Disp(i,y));
+        //     //_Slider(i);
+        //     //StartCoroutine(CutIn_Slide(i,y));
+        //     y += 2;
+        // }
+        //
+        StartCoroutine(CutIn_Disp(0,1));
+        //StartCoroutine(CutIn_Fin(3));
 
     }
 
     private void CutIn_Init()
     {
+       
         for (int i = 0; i < Dialogue.Count; i++)
         {
             Dialogue[i].text = _roulette.GetDialogue(i);
@@ -39,19 +86,23 @@ public class CutIn_Manager : MonoBehaviour
 
         for (int y = 0; y < Character.Count; y++)
         {
+            Debug.Log(_roulette.get_D_ID()[y]);
             Character[y].sprite = Resources.Load<Sprite>("Slot/" + _roulette.GetCharacterId(y) + "_Stand");
         }
-
+        
         for (int x = 0; x < Frame.Count; x++)
         {
-            pos[x] = Character[x].transform.localPosition;
+            pos.Add(Frame[x].transform.localPosition);
+            // pos[x] = Character[x].transform.localPosition;
+            // Debug.Log(pos[x]);
         }
-        pos[Frame.Count] = Character[Frame.Count].transform.localPosition;
+        pos[Frame.Count-1] = Frame[Frame.Count-1].transform.localPosition;
     }
 
     IEnumerator CutIn_Disp(int num,float sec)
     {
-        yield return new WaitForSeconds(sec);
+        
+        
         Frame[num].enabled = true;
         Dialogue[num].enabled = true;
         if (num == 2)
@@ -60,26 +111,67 @@ public class CutIn_Manager : MonoBehaviour
         }
 
         Character[num].enabled = true;
+        yield return new WaitForSeconds(sec);
+        if (num < D_Slider.Length)
+        {
+            D_Slider[num] = true;
+        }
+        
+       
+    }
+
+    void _Slider(int num)
+    {
+        
+        if (num > 0)
+        {
+            Vector3 target = new Vector3(pos[num-1].x, 339f, pos[num-1].z);
+            
+            if (Vector3.Distance(Frame[num-1].transform.localPosition, target) > 0.001f)
+            {
+                float step =  300 * Time.deltaTime; // calculate distance to move
+                Frame[num-1].transform.localPosition = Vector3.MoveTowards(Frame[num-1].transform.localPosition, target, step);
+            }
+            else
+            {
+                D_Slider_Done[num - 1] = true;
+            }
+
+        }
     }
     IEnumerator CutIn_Slide(int num,float sec)
     {
-        if (num < 2)
+        
+        if (num > 0)
         {
-            yield return new WaitForSeconds(sec);
-            Character[num].transform.localPosition = new Vector3(pos[num].x, pos[num].y + 278);
-            pos[num] = Character[num].transform.localPosition;
+            Vector3 target = new Vector3(pos[num-1].x, 339f, pos[num-1].z);
+            
+            while (Vector3.Distance(Frame[num-1].transform.localPosition, target) > 0.001f)
+            {
+                float step =  1 * Time.deltaTime; // calculate distance to move
+                Frame[num-1].transform.localPosition = Vector3.MoveTowards(Frame[num-1].transform.localPosition, target, step);
+            }
+            yield return new WaitForSeconds(0);
+            // yield return new WaitForSeconds(sec);
+            // //Frame[num].transform.localPosition = new Vector3(pos[num].x, pos[num].y + 278);
+            // Frame[num].transform.localPosition = new Vector3(pos[num].x, pos[num].y);
+            // pos[num] = Frame[num].transform.localPosition;
+
         }
     }
 
     IEnumerator CutIn_Fin(float sec)
     {
+        // Frame[i].enabled = false;
+        // Character[i].enabled = false;
+        // Dialogue[i].enabled = false;
         yield return new WaitForSeconds(sec);
         for (int i = 0; i < Frame.Count; i++)
         {
             Frame[i].enabled = false;
             Character[i].enabled = false;
             Dialogue[i].enabled = false;
-            Character[i].transform.localPosition = new Vector3(pos[Frame.Count].x,pos[Frame.Count].y);
+            Frame[i].transform.localPosition = new Vector3(pos[Frame.Count-1].x,pos[Frame.Count-1].y);
         }
     }
 }
--- Assets/Script/Roulette/Roulette.cs
+++ Assets/Script/Roulette/Roulette.cs
@@ -25,7 +25,7 @@ public class Roulette : MonoBehaviour
     [SerializeField] private int _AliveCharacterID1;
     [SerializeField] private int _AliveCharacterID2;
     [SerializeField] private int _AliveCharacterID3;
-    
+
     [SerializeField] private int _AliveBoss;
 
     [SerializeField] private GameObject Reel_l;
@@ -37,6 +37,9 @@ public class Roulette : MonoBehaviour
     [SerializeField] private GameObject PauseButton;
 
     [SerializeField] private CutIn_Manager CutIn;
+
+    [SerializeField] private List<int> Character_D_ID = new List<int>(3);
+
     //初期化
     private void Initialize()
     {
@@ -61,22 +64,22 @@ public class Roulette : MonoBehaviour
         for (int i = 0; i < data.Dialogue.Count; i++)
         {
             //Debug.Log(data.StageList[i].MapID);
-            
+
             //DialogueテーブルのCharacterID1,2,3に現在生きているバトルシーンに居るオブジェクトのIDがあるテーブルだけを抜き出す
-            if ((data.Dialogue[i].CharacterID1 == _AliveCharacterID1 || 
-                data.Dialogue[i].CharacterID1 == _AliveCharacterID2 || 
-                data.Dialogue[i].CharacterID1 == _AliveCharacterID3) &&(
-                data.Dialogue[i].CharacterID2 == _AliveCharacterID1 ||
-                data.Dialogue[i].CharacterID2 == _AliveCharacterID2 ||
-                data.Dialogue[i].CharacterID2 == _AliveCharacterID3 ||
-                data.Dialogue[i].CharacterID2 == 0 ||
-                data.Dialogue[i].CharacterID2 == null )&&(
-                data.Dialogue[i].CharacterID3 == _AliveCharacterID1 ||
-                data.Dialogue[i].CharacterID3 == _AliveCharacterID2 ||
-                data.Dialogue[i].CharacterID3 == _AliveCharacterID3 ||
-                data.Dialogue[i].CharacterID3 == 0 ||
-                data.Dialogue[i].CharacterID3 == null )
-                )
+            if ((data.Dialogue[i].CharacterID1 == _AliveCharacterID1 ||
+                 data.Dialogue[i].CharacterID1 == _AliveCharacterID2 ||
+                 data.Dialogue[i].CharacterID1 == _AliveCharacterID3) && (
+                    data.Dialogue[i].CharacterID2 == _AliveCharacterID1 ||
+                    data.Dialogue[i].CharacterID2 == _AliveCharacterID2 ||
+                    data.Dialogue[i].CharacterID2 == _AliveCharacterID3 ||
+                    data.Dialogue[i].CharacterID2 == 0 ||
+                    data.Dialogue[i].CharacterID2 == null) && (
+                    data.Dialogue[i].CharacterID3 == _AliveCharacterID1 ||
+                    data.Dialogue[i].CharacterID3 == _AliveCharacterID2 ||
+                    data.Dialogue[i].CharacterID3 == _AliveCharacterID3 ||
+                    data.Dialogue[i].CharacterID3 == 0 ||
+                    data.Dialogue[i].CharacterID3 == null)
+            )
             {
                 DiaLine.Add(i);
                 DiaIDList.Add(data.Dialogue[i].DialogueID);
@@ -84,6 +87,7 @@ public class Roulette : MonoBehaviour
                 DiaDisplay.Add(data.Dialogue[i].Dialogue);
                 Rcount++;
             }
+
             if (_AliveBoss == data.Dialogue[i].CharacterID1)
             {
                 DiaLine.Add(i);
@@ -92,9 +96,9 @@ public class Roulette : MonoBehaviour
                 DiaDisplay.Add(data.Dialogue[i].Dialogue);
                 Rcount++;
             }
-            
+
         }
-        
+
         //スロットの表示
         Slot.SetActive(true);
         PauseButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
@@ -122,66 +126,66 @@ public class Roulette : MonoBehaviour
     //Slotのメイン判定部
     public void Roul_main()
     {
-       Roul();
-       
-       //1つ目のセリフを抽出する
-       target1 = Random.Range(0, Con_0_1.Count);
-       target1 = Con_0_1[target1];
-       
-       //２つ目のセリフを抽出する
-       //Continueが１か２なら次のセリフを２つ目のセリフにする
-       if (data.Dialogue[target1].Continue == 1 && data.Dialogue[target1+1].Continue == 2)
-       {
-           target2 = target1 + 1;
-       }
-       else //Continueが0か1なら次のセリフをランダムで決める
-       {
-           target2 = Random.Range(0, Con_0_1.Count);
-           target2 = Con_0_1[target2];
-           while (target1 == target2)
-           {
-               target2 = Random.Range(0, Con_0_1.Count);
-               target2 = Con_0_1[target2];
-           }
-       }
+        Roul();
 
-       //３つ目のセリフを抽出する
-       //２つ目のセリフのContinueが1か２ならその続きのセリフを３つ目のセリフにする
-       if ((data.Dialogue[target2].Continue == 2 && data.Dialogue[target2+1].Continue == 3) ||
-           (data.Dialogue[target2].Continue == 1 && data.Dialogue[target2+1].Continue == 2))
-       {
-           target3 = target2 + 1;
-       }
-       else //上記の条件に当てはまらない場合３つ目のセリフをランダムで抽出する
-       {
-           target3 = Random.Range(0, Con_0_1.Count);
-           target3 = Con_0_1[target3];
-           while (target1 == target3 || target3 == target2)
-           {
-               target3 = Random.Range(0, Con_0_1.Count);
-               target3 = Con_0_1[target3];
-           }
-       }
-       
-       //抽出したセリフの確認
-       Debug.Log(data.Dialogue[target1].Dialogue);
-       Debug.Log(data.Dialogue[target2].Dialogue);
-       Debug.Log(data.Dialogue[target3].Dialogue);
+        //1つ目のセリフを抽出する
+        target1 = Random.Range(0, Con_0_1.Count);
+        target1 = Con_0_1[target1];
 
-       //リールの操作に関するコルーチン
-       StartCoroutine("DisplaySlot", 2);
-       Debug.Log("スタート");
-       StartCoroutine("ReelStopL", 4);
-       StartCoroutine("ReelStopC", 5);
-       StartCoroutine("ReelStopR", 6);
-       StartCoroutine("ReelFin", 15);
+        //２つ目のセリフを抽出する
+        //Continueが１か２なら次のセリフを２つ目のセリフにする
+        if (data.Dialogue[target1].Continue == 1 && data.Dialogue[target1 + 1].Continue == 2)
+        {
+            target2 = target1 + 1;
+        }
+        else //Continueが0か1なら次のセリフをランダムで決める
+        {
+            target2 = Random.Range(0, Con_0_1.Count);
+            target2 = Con_0_1[target2];
+            while (target1 == target2)
+            {
+                target2 = Random.Range(0, Con_0_1.Count);
+                target2 = Con_0_1[target2];
+            }
+        }
+
+        //３つ目のセリフを抽出する
+        //２つ目のセリフのContinueが1か２ならその続きのセリフを３つ目のセリフにする
+        if ((data.Dialogue[target2].Continue == 2 && data.Dialogue[target2 + 1].Continue == 3) ||
+            (data.Dialogue[target2].Continue == 1 && data.Dialogue[target2 + 1].Continue == 2))
+        {
+            target3 = target2 + 1;
+        }
+        else //上記の条件に当てはまらない場合３つ目のセリフをランダムで抽出する
+        {
+            target3 = Random.Range(0, Con_0_1.Count);
+            target3 = Con_0_1[target3];
+            while (target1 == target3 || target3 == target2)
+            {
+                target3 = Random.Range(0, Con_0_1.Count);
+                target3 = Con_0_1[target3];
+            }
+        }
+
+        //抽出したセリフの確認
+        Debug.Log(data.Dialogue[target1].Dialogue);
+        Debug.Log(data.Dialogue[target2].Dialogue);
+        Debug.Log(data.Dialogue[target3].Dialogue);
+
+        //リールの操作に関するコルーチン
+        StartCoroutine("DisplaySlot", 2);
+        Debug.Log("スタート");
+        StartCoroutine("ReelStopL", 4);
+        StartCoroutine("ReelStopC", 5);
+        StartCoroutine("ReelStopR", 6);
+        StartCoroutine("ReelFin", 15);
 
     }
 
     //遅延を入れてリールスタート
     IEnumerator DisplaySlot(float sec)
     {
-        
+
         yield return new WaitForSeconds(sec);
 
         Reel_l.GetComponent<ReelController>().first = false;
@@ -192,24 +196,26 @@ public class Roulette : MonoBehaviour
 
     //各リールを0.5秒ごとに止める
     IEnumerator ReelStopL(float sec)
-    {   
+    {
         yield return new WaitForSeconds(sec);
         Debug.Log("SlotL");
         Reel_l.GetComponent<ReelController>().Reel_Stop();
     }
+
     IEnumerator ReelStopC(float sec)
     {
         yield return new WaitForSeconds(sec);
         Debug.Log("SlotC");
         Reel_C.GetComponent<ReelController>().Reel_Stop();
     }
+
     IEnumerator ReelStopR(float sec)
     {
         yield return new WaitForSeconds(sec);
         Debug.Log("SlotR");
         Reel_R.GetComponent<ReelController>().Reel_Stop();
     }
-    
+
     //スロットを非表示にする
     IEnumerator ReelFin(float sec)
     {
@@ -223,7 +229,7 @@ public class Roulette : MonoBehaviour
         Debug.Log("Fin");
 
         CutIn.Dialogue_CutIn();
-        
+
         PauseButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
     }
 
@@ -235,34 +241,97 @@ public class Roulette : MonoBehaviour
             case 0:
                 if (data.Dialogue[target1].Continue == 1 || data.Dialogue[target1].Continue == 0)
                 {
-                    value = TargetID(target1);
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target1].CharacterID1 == _battleManager._AliveID[i])
+                        {
+                            value = i;
+                            Character_D_ID[0] = data.Dialogue[target1].CharacterID1;
+                            break;
+                        }
+                    }
+
                 }
+
+                //value = (target1 / 1000) - 1;
                 break;
             case 1:
                 if (data.Dialogue[target2].Continue == 2)
                 {
-                    value = TargetID(target2);
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target2].CharacterID2 == _battleManager._AliveID[i])
+                        {
+                            value = i;
+                            Character_D_ID[1] = data.Dialogue[target2].CharacterID2;
+
+                            break;
+                        }
+                    }
+
                 }
                 else
                 {
-                    value = TargetID(target2);
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target2].CharacterID1 == _battleManager._AliveID[i])
+                        {
+                            value = i;
+                            Character_D_ID[1] = data.Dialogue[target2].CharacterID1;
+
+                            break;
+                        }
+                    }
                 }
+
+                //value = (target2 / 1000) - 1;
                 break;
             case 2:
                 if (data.Dialogue[target3].Continue == 3)
                 {
-                    value = TargetID(target3);
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target3].CharacterID3 == _battleManager._AliveID[i])
+                        {
+                            Character_D_ID[2] = data.Dialogue[target3].CharacterID3;
+
+                            value = i;
+                            break;
+                        }
+                    }
+
                 }
                 else if (data.Dialogue[target3].Continue == 2)
                 {
-                    value = TargetID(target3);
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target3].CharacterID2 == _battleManager._AliveID[i])
+                        {
+                            Character_D_ID[2] = data.Dialogue[target3].CharacterID2;
+
+                            value = i;
+                            break;
+                        }
+                    }
                 }
-                else {
-                    value = TargetID(target3);
+                else
+                {
+                    for (int i = 0; i < _battleManager._AliveID.Count; i++)
+                    {
+                        if (data.Dialogue[target3].CharacterID1 == _battleManager._AliveID[i])
+                        {
+                            Character_D_ID[2] = data.Dialogue[target3].CharacterID1;
+
+                            value = i;
+                            break;
+                        }
+                    }
                 }
+
+                //value = (target3 / 1000) - 1;
                 break;
         }
-        
+
         return value;
     }
 
@@ -276,9 +345,10 @@ public class Roulette : MonoBehaviour
                 break;
             }
         }
+
         return num;
     }
-    
+
     public string GetDialogue(int num)
     {
         string serif;
@@ -297,26 +367,33 @@ public class Roulette : MonoBehaviour
                 serif = "error";
                 break;
         }
+
         return serif;
     }
 
-    public int GetCharacterId(int num)
+    public List<int> get_D_ID()
     {
-        switch (num)
-        {
-            case 0:
-                num = data.Dialogue[target1].CharacterID1;
-                break;
-            case 1:
-                num = data.Dialogue[target1].CharacterID1;
-                break;
-            case 2:
-                num = data.Dialogue[target1].CharacterID1;
-                break;
-            default:
-                num = 0;
-                break;
-        }
-        return num;
+        return Character_D_ID;
+    }
+
+public int GetCharacterId(int num)
+    {
+        return Character_D_ID[num];
+        // switch (num)
+        // {
+        //     case 0:
+        //         num = data.Dialogue[target1].CharacterID1;
+        //         break;
+        //     case 1:
+        //         num = data.Dialogue[target1].CharacterID1;
+        //         break;
+        //     case 2:
+        //         num = data.Dialogue[target1].CharacterID1;
+        //         break;
+        //     default:
+        //         num = 0;
+        //         break;
+        // }
+        // return num;
     }
 }
