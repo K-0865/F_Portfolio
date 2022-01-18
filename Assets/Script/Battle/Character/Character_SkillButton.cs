f8cc418666d76ca1b8c1471c711e900dcad86598
KDavidx <77654793+KDavidx@users.noreply.github.com>
2022-01-18 21:08:32 +0900

    at least show icon in skill bar

--- Assets/Script/Battle/Character/Character_SkillButton.cs
+++ Assets/Script/Battle/Character/Character_SkillButton.cs
@@ -13,10 +13,17 @@ public class Character_SkillButton : MonoBehaviour
         [SerializeField] public List<Skill_Data> C_Position;
         [SerializeField] private List<GameObject> Button;
         [SerializeField] private List<GameObject> HPBar;
+        private skil_icon _skilIcon;
+
+        private void Start()
+        {
+                _skilIcon = GetComponent<skil_icon>();
+        }
+
         public void set_Animator_Character(Animator a)
         {
                 All_Character.Add(a);
-                //Button[All_Character.Count-1].SetActive(true);
+                Button[All_Character.Count-1].SetActive(true);
                 
                 // hp.maxValue = All_Character_Data[All_Character.Count - 1]._maxHp;
                 // hp.value = All_Character_Data[All_Character.Count - 1]._hp;
@@ -30,6 +37,9 @@ public class Character_SkillButton : MonoBehaviour
                 Debug.Log("");
                 hp._slider.maxValue = All_Character_Data[All_Character_Data.Count - 1]._maxHp;
                 hp._slider.value = All_Character_Data[All_Character_Data.Count - 1]._hp;
+                _skilIcon._icon[All_Character_Data.Count - 1].sprite =
+                        Resources.Load<Sprite>("Battle/UI/icon/" + a._ID + "_icon");
+
         }
         public void set_Movement_Character(Character_Movement a)
         {
@@ -43,6 +53,7 @@ public class Character_SkillButton : MonoBehaviour
 
         public void Use_Skill(int num)
         {
+                Debug.Log(num);
                 if (!All_Character_Movement[num].isUseSP)
                 {
                         All_Character_Movement[num].isUseSP = true;
---
+++ Assets/Script/Battle/Map/skil_icon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skil_icon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Image> _icon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
